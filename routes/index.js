50d12665904c7ff0a015b13f3d3c20e39fd3908c
tomas <Homero_77>
2023-04-18 16:59:55 -0300

    test

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
-const public  = require('./public')
+const public = require('./public')
 const private = require('./private')
 
 const router = Router()
+
+router.get('/', (req, res) => {
+    res.send({ ok: true })
+})
 
 router.use('/public', public)
 router.use('/private', private)
