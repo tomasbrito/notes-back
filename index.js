8dee42485b69873932116f8ceeca93472e91e39d
tomas <Homero_77>
2023-04-20 18:46:49 -0300

    cors fixed

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const router = require('./routes')
 const app = express()
 const port = process.env.port || 80
 
+
+// Configurar las opciones de CORS
+const corsOptions = {
+  origin: true,
+  methods: ['GET', 'POST'],
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+// Usar el middleware de CORS
+app.use(cors(corsOptions));
+
 app.use(
     express.urlencoded({
         extended: true
@@ -16,7 +28,6 @@ app.use(express.json({
 
 app.use(router)
 
-app.use(cors())
 
 app.listen(port, () => {
     console.log(`Ejecutando en puerto ${port}`)
